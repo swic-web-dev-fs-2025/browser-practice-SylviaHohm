531ce35b290f0fab590b06727803980a8a1e477e
Sylvia Hohm <sylviamhohm@gmail.com>
2025-11-02 22:30:39 -0600

    added color change after 5 clicks to main.js

--- src/main.js
+++ src/main.js
@@ -12,12 +12,20 @@ function updateGreeting() {
   clickCount++;
   greetingElement.textContent = "Hello, Browser JavaScript!";
   countElement.textContent = `Button clicks: ${clickCount}`;
+  // Turn text red after more than 5 clicks, otherwise reset color
+  if (clickCount > 4) {
+    greetingElement.style.color = "red";
+  } else {
+    greetingElement.style.color = "";
+  }
 }
 
 function resetGreeting() {
   clickCount = 0;
   greetingElement.textContent = "Hello, Console JavaScript!";
   countElement.textContent = `Button clicks: ${clickCount}`;
+  // Reset color when page is reset
+  greetingElement.style.color = "";
 }
 
 // Event listener - this is new! Responds to user interaction
